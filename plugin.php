912f1d0e5bcc69f5cd2717c9ec0aadf7487f2658
Nathan Rice <ncrice@gmail.com>
2015-03-03 17:25:18 -0500

    Standards.

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Simple URLs
 Plugin URI: http://www.studiopress.com/plugins/simple-urls
 Description: Simple URLs is a complete URL management system that allows you create, manage, and track outbound links from your site by using custom post types and 301 redirects.
-Version: 0.9.4
+Version: 0.9.5
 Author: Nathan Rice
 Author URI: http://www.nathanrice.net/
 */
@@ -11,19 +11,16 @@ class SimpleURLs {
 	
 	// Constructor
 	function __construct() {
+
 		//register_activation_hook( __FILE__, 'flush_rewrite_rules' );
-		
+
 		add_action( 'init', array( &$this, 'register_post_type' ) );
 		add_action( 'manage_posts_custom_column', array( &$this, 'columns_data' ) );
 		add_filter( 'manage_edit-surl_columns', array( &$this, 'columns_filter' ) );
 		add_action( 'admin_menu', array( &$this, 'add_meta_box' ) );
 		add_action( 'save_post', array( &$this, 'meta_box_save' ), 1, 2 );
 		add_action( 'template_redirect', array( &$this, 'count_and_redirect' ) );
-	}
-	
-	// PHP4 Constructor
-	function SimpleURLs() {
-		$this->__construct();
+
 	}
 	
 	function register_post_type() {
@@ -31,24 +28,25 @@ class SimpleURLs {
 		register_post_type( 'surl',
 			array(
 				'labels' => array(
-					'name' => __( 'Simple URLs' ),
-					'singular_name' => __( 'URL' ),
-					'add_new' => __( 'Add New' ),
-					'add_new_item' => __( 'Add New URL' ),
-					'edit' => __( 'Edit' ),
-					'edit_item' => __( 'Edit URL' ),
-					'new_item' => __( 'New URL' ),
-					'view' => __( 'View URL' ),
-					'view_item' => __( 'View URL' ),
-					'search_items' => __( 'Search URL' ),
-					'not_found' => __( 'No URLs found' ),
+					'name'               => __( 'Simple URLs' ),
+					'singular_name'      => __( 'URL' ),
+					'add_new'            => __( 'Add New' ),
+					'add_new_item'       => __( 'Add New URL' ),
+					'edit'               => __( 'Edit' ),
+					'edit_item'          => __( 'Edit URL' ),
+					'new_item'           => __( 'New URL' ),
+					'view'               => __( 'View URL' ),
+					'view_item'          => __( 'View URL' ),
+					'search_items'       => __( 'Search URL' ),
+					'not_found'          => __( 'No URLs found' ),
 					'not_found_in_trash' => __( 'No URLs found in Trash' )
 				),
-				'public' => true,
-				'query_var' => true,
+				'public'        => true,
+				'query_var'     => true,
 				'menu_position' => 20,
-				'supports' => array( 'title' ),
-				'rewrite' => array( 'slug' => 'go', 'with_front' => false )
+				'supports'      => array( 'title' ),
+				'rewrite'       => array( 'slug' => 'go', 'with_front' => false )
+
 			)
 		);
 		
@@ -57,11 +55,11 @@ class SimpleURLs {
 	function columns_filter( $columns ) {
 		
 		$columns = array(
-			'cb' => '<input type="checkbox" />',
-			'title' => __('Title'),
-			'url' => __('Redirect to'),
-			'permalink' => __('Permalink'),
-			'clicks' => __('Clicks')
+			'cb'        => '<input type="checkbox" />',
+			'title'     => __( 'Title' ),
+			'url'       => __( 'Redirect to' ),
+			'permalink' => __( 'Permalink' ),
+			'clicks'    => __( 'Clicks' ),
 		);
 		
 		return $columns;
@@ -72,33 +70,34 @@ class SimpleURLs {
 		
 		global $post;
 		
-		$url = get_post_meta($post->ID, '_surl_redirect', true);
-		$count = get_post_meta($post->ID, '_surl_count', true);
+		$url   = get_post_meta( $post->ID, '_surl_redirect', true );
+		$count = get_post_meta( $post->ID, '_surl_count', true );
 		
-		if ( $column == 'url' ) {
+		if ( 'url' == $column ) {
 			echo make_clickable( esc_url( $url ? $url : '' ) );
 		}
-		elseif ( $column == 'permalink' ) {
+		elseif ( 'permalink' == $column ) {
 			echo make_clickable( get_permalink() );
 		}
-		elseif ( $column == 'clicks' ) {
+		elseif ( 'clicks' == $column ) {
 			echo esc_html( $count ? $count : 0 );
 		}
 		
 	}
 	
 	function add_meta_box() {
-		add_meta_box('surl', __('URL Information', 'surl'), array( &$this, 'meta_box' ), 'surl', 'normal', 'high');
+		add_meta_box( 'surl', __( 'URL Information', 'surl' ), array( $this, 'meta_box' ), 'surl', 'normal', 'high' );
 	}
 	
 	function meta_box() {
+
 		global $post;
-		
+
 		printf( '<input type="hidden" name="_surl_nonce" value="%s" />', wp_create_nonce( plugin_basename(__FILE__) ) );
-		
-		printf( '<p><label for="%s">%s</label></p>', '_surl_redirect', __('Redirect URI', 'surl') );
+
+		printf( '<p><label for="%s">%s</label></p>', '_surl_redirect', __( 'Redirect URI', 'surl' ) );
 		printf( '<p><input style="%s" type="text" name="%s" id="%s" value="%s" /></p>', 'width: 99%;', '_surl_redirect', '_surl_redirect', esc_attr( get_post_meta( $post->ID, '_surl_redirect', true ) ) );
-		
+
 		$count = isset( $post->ID ) ? get_post_meta($post->ID, '_surl_count', true) : 0;
 		printf( '<p>This URL has been accessed <b>%d</b> times.', esc_attr( $count ) );
 		
@@ -109,8 +108,9 @@ class SimpleURLs {
 		$key = '_surl_redirect';
 		
 		//	verify the nonce
-		if ( !isset($_POST['_surl_nonce']) || !wp_verify_nonce( $_POST['_surl_nonce'], plugin_basename(__FILE__) ) )
+		if ( ! isset( $_POST['_surl_nonce'] ) || ! wp_verify_nonce( $_POST['_surl_nonce'], plugin_basename( __FILE__ ) ) ) {
 			return;
+		}
 			
 		//	don't try to save the data under autosave, ajax, or future post.
 		if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) return;
@@ -118,17 +118,17 @@ class SimpleURLs {
 		if ( defined('DOING_CRON') && DOING_CRON ) return;
 
 		//	is the user allowed to edit the URL?
-		if ( ! current_user_can( 'edit_posts' ) || $post->post_type != 'surl' )
+		if ( ! current_user_can( 'edit_posts' ) || 'surl' != $post->post_type )
 			return;
 			
-		$value = isset( $_POST[$key] ) ? $_POST[$key] : '';
+		$value = isset( $_POST[ $key ] ) ? $_POST[ $key ] : '';
 		
 		if ( $value ) {
 			//	save/update
-			update_post_meta($post->ID, $key, $value);
+			update_post_meta( $post->ID, $key, $value );
 		} else {
 			//	delete if blank
-			delete_post_meta($post->ID, $key);
+			delete_post_meta( $post->ID, $key );
 		}
 		
 	}
@@ -136,19 +136,20 @@ class SimpleURLs {
 	
 	function count_and_redirect() {
 		
-		if ( !is_singular('surl') )
+		if ( ! is_singular( 'surl' ) ) {
 			return;
+		}
 
 		global $wp_query;
 		
 		// Update the count
-		$count = isset( $wp_query->post->ID ) ? get_post_meta($wp_query->post->ID, '_surl_count', true) : 0;
+		$count = isset( $wp_query->post->ID ) ? get_post_meta( $wp_query->post->ID, '_surl_count', true ) : 0;
 		update_post_meta( $wp_query->post->ID, '_surl_count', $count + 1 );
 
 		// Handle the redirect
-		$redirect = isset( $wp_query->post->ID ) ? get_post_meta($wp_query->post->ID, '_surl_redirect', true) : '';
+		$redirect = isset( $wp_query->post->ID ) ? get_post_meta( $wp_query->post->ID, '_surl_redirect', true ) : '';
 
-		if ( !empty( $redirect ) ) {
+		if ( ! empty( $redirect ) ) {
 			wp_redirect( esc_url_raw( $redirect ), 301);
 			exit;
 		}
@@ -161,4 +162,4 @@ class SimpleURLs {
 	
 }
 
-$SimpleURLs = new SimpleURLs;
+new SimpleURLs;
